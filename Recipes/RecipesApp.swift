3e3392d18be184f32ecdc5e1ba40ac805874a26e
JCrai <jake.craig24@gmail.com>
2025-04-12 19:18:20 -0400

    Initial Commit

---
+++ Recipes/RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipesApp.swift
+//  Recipes
+//
+//  Created by Jacob Craig on 4/12/25.
+//
+
+import SwiftUI
+
+@main
+struct RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
